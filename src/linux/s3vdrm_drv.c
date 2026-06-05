9eda417e1b02bb6bd3429c148f68671031307ccf
kopi9999 <przem.gda@gmail.com>
2026-06-05 21:57:24 +0200

    added licensing info to files

--- src/linux/s3vdrm_drv.c
+++ src/linux/s3vdrm_drv.c
@@ -1,3 +1,9 @@
+/*
+ * SPDX_License_identifier: GPL-3.0-or-later
+ * Copyright (c) 2026 kopi9999
+ * This file is a part of s3vdrm project.
+ */
+
 #include <linux/module.h>
 #include <linux/pci.h>
 
--- src/linux/s3vdrm_drv.h
+++ src/linux/s3vdrm_drv.h
@@ -1,3 +1,10 @@
+
+/*
+ * SPDX_License_identifier: GPL-3.0-or-later
+ * Copyright (c) 2026 kopi9999
+ * This file is a part of s3vdrm project.
+ */
+
 #ifndef S3VDRM_DRV_H
 #define S3VDRM_DRV_H
 
