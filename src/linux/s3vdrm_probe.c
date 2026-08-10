40ee6f5016337e024191f591f14e1d917f07a287
kopi9999 <przem.gda@gmail.com>
2026-08-10 21:07:44 +0200

    improved log visuals

--- src/linux/s3vdrm_probe.c
+++ src/linux/s3vdrm_probe.c
@@ -8,23 +8,23 @@
 #include "../s3vdrm_dev_ids.h"
 
 static void s3vdrm_log_config(struct s3v_status *status) {
-  pr_info("[s3vdrm] Detected supported device, printing configuration:");
+  pr_info("s3vdrm: Detected supported device, printing configuration:");
   
   switch (status->device_id) {
     case S3VDRM_VIRGE_DEV_ID:
-      pr_info("[s3vdrm] Model: S3 Virge 325");
+      pr_info("s3vdrm: Model: S3 Virge 325");
       break;
     case S3VDRM_VIRGE_DX_GX_DEV_ID:
-      pr_info("[s3vdrm] Model: S3 Virge DX or S3 Virge GX");
+      pr_info("s3vdrm: Model: S3 Virge DX or S3 Virge GX");
       break;
   }
 
   switch (status->vram_size) {
   case 0x400000:
-    pr_info("[s3vdrm] VRAM size: 4M");
+    pr_info("s3vdrm: VRAM size: 4M");
     break;
   case 0x200000:
-    pr_info("[s3vdrm] VRAM size: 2M");
+    pr_info("s3vdrm: VRAM size: 2M");
     break;
   }
 }
@@ -36,10 +36,10 @@ int s3vdrm_probe(void __iomem *base, struct s3v_status *status, u16 device_id) {
   if (error) {
     switch (error) {
       case PROBE_CARD_UNRESPONSIVE:
-	pr_crit("[s3vdrm] Device MMIO could not been enabled.");
+	pr_crit("s3vdrm: Device MMIO could not been enabled.");
 	break;
       case PROBE_BAD_VRAM:
-	pr_crit("[s3vdrm] VRAM size could not been read.");
+	pr_crit("s3vdrm: VRAM size could not been read.");
 	break;
       default: break;
     }
